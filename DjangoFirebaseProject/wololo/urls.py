2a223cb1e1faf8515e1657cf58b1d39600c09c0b
kutayCyberratss <salty@cyberratss.london>
2019-01-10 12:30:07 +0000

    pyrebase4 added, firebase auth added

--- DjangoFirebaseProject/wololo/urls.py
+++ DjangoFirebaseProject/wololo/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('game', views.game, name='game'),
+    path('register', views.register, name='register'),
 ]
--- DjangoFirebaseProject/wololo/views.py
+++ DjangoFirebaseProject/wololo/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
-
+import pyrebase
+config = ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+  ***REMOVED***
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 # Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        email=request.POST.get("email")
+        password = request.POST.get("pass")
+
+        user_add = auth.create_user_with_email_and_password(email, password)
+        return render(request, 'game.html',***REMOVED***'email':email***REMOVED***)
+    return render(request, 'register.html')
+
 def index(request):
     # now = datetime.datetime.now()
     # html = "<html><body>It is now %s.</body></html>" % now
     return render(request, 'layout.html')
+
 def game(request):
     # now = datetime.datetime.now()
     # html = "<html><body>It is now %s.</body></html>" % now
+
     return render(request, 'game.html')
