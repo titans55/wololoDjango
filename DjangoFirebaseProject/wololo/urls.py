018c11aea6dc3d0fe9b5e32b5dfb8548fade5d78
kutay <kutayaslanfb@hotmail.com>
2019-04-11 06:56:51 +0300

    changed file and folder names in views folder

--- DjangoFirebaseProject/wololo/urls.py
+++ DjangoFirebaseProject/wololo/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 # from . import views
-from wololo.views.afterLoginViews.buildingViews import barracksView
-from wololo.views.afterLoginViews import mapView, villagesViews, clansView, reportsView
-from wololo.views.beforeLoginViews import selectingRegionView, landingView, registerView
+from wololo.views.afterLogin.buildings import barracksView
+from wololo.views.afterLogin import mapView, villagesView, clansView, reportsView
+from wololo.views.beforeLogin import selectingRegionView, landingView, registerView
 from wololo.views import auth
 urlpatterns = [
     path('', landingView.landingPage, name='landingPage'),
@@ -12,8 +12,8 @@ urlpatterns = [
     path('game/selectRegion', selectingRegionView.selectRegionOnFirstLoginView, name='selectRegion'),
 
 
-    path('game', villagesViews.villages, name='myVillage'),
-    path('game/<int:village_index>', villagesViews.villages),
+    path('game', villagesView.villages, name='myVillage'),
+    path('game/<int:village_index>', villagesView.villages),
 
     path('game/map', mapView.map, name='map'),
     path('game/<int:village_index>/map', mapView.map),
@@ -27,9 +27,9 @@ urlpatterns = [
     path('game/clans', clansView.clans, name='clans'),
     path('game/reports', reportsView.reports, name='reports'),
 
-
-    path('game/upgrade', villagesViews.upgrade, name='upgrade'),
-    path('game/cancelUpgrade', villagesViews.cancelUpgrade, name='cancelUpgrade'),
+    #AJAX CALL
+    path('game/upgrade', villagesView.upgrade, name='upgrade'),
+    path('game/cancelUpgrade', villagesView.cancelUpgrade, name='cancelUpgrade'),
 
     #FORM CALL
     path('game/selectingRegion', selectingRegionView.selectingRegion),
